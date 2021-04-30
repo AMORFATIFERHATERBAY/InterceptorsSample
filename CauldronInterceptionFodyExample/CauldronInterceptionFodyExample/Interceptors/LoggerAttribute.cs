b2cc9dd6bd473da9bacf2976932e96c8ccb480b5
AmorFatiBA <you@example.com>
2021-04-30 14:40:53 +0300

    test1 commit

---
+++ CauldronInterceptionFodyExample/CauldronInterceptionFodyExample/Interceptors/LoggerAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Cauldron.Interception;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace CauldronInterceptionFodyExample.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class LoggerAttribute : Attribute, IMethodInterceptor
+    {
+        private string methodName;
+
+
+        public void OnEnter(Type declaringType, object instance, MethodBase methodbase, object[] values)
+        {
+            this.methodName = methodbase.Name;
+            this.AppendToFile($"Enter -> {declaringType.Name} {methodbase.Name} {string.Join(" ", values)}");
+        }
+
+        public void OnException(Exception e) => this.AppendToFile($"Exception -> {e.Message}");
+
+        public void OnExit() => this.AppendToFile($"Exit -> {this.methodName}");
+
+        private void AppendToFile(string line)
+        {
+            File.AppendAllLines("log.txt", new string[] { line });
+            Console.WriteLine(">> " + line);
+        }
+    }
+}
---
+++ CauldronInterceptionFodyExample/CauldronInterceptionFodyExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using CauldronInterceptionFodyExample.Interceptors;
+using System;
+
+namespace CauldronInterceptionFodyExample
+{
+    [OnPropertySet(nameof(ExecuteMe))]
+    public class PropertySetterTestClass
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+
+        private void ExecuteMe(string propertyName, object newValue) =>
+            Console.WriteLine($"The property '{propertyName}' has a new value: '{newValue ?? ""}'");
+    }
+
+    [Logger]
+    internal class Program
+    {
+        private static int Add(int a, int b) => a + b;
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Add(5, 20));
+
+            var sampleClass = new PropertySetterTestClass
+            {
+                BookName = "50 shades of C#",
+                BookId = 23432
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
